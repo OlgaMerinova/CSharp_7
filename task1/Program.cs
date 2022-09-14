e54603add8713b783112cefa047cba30686d6dd2
olja4567 <olja4567@yandex.ru>
2022-09-14 11:11:21 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными (тип double) числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+double [,] arr = new double [3, 4];
+Random rnd = new Random();
+
+for (int i=0; i<3; i++)
+{
+    for (int j=0; j<4; j++)
+    {
+        arr[i, j] = rnd.NextDouble();
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
